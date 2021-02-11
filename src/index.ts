127c829150a1955950ec9d694820e56fcb282fb3
FAL <falworks.contact@gmail.com>
2021-02-11 23:07:02 +0900

    feat: expose config

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export type { AliasRecord } from "./alias";
 export { scan } from "./scan.js";
 export { parse } from "./parse.js";
 export * from "./convert/index.js";
+export { config } from "./config.js";
