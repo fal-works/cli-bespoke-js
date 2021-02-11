10eeb720fb16d51a98a9fbff409942354ef9a75d
FAL <falworks.contact@gmail.com>
2021-02-11 22:45:53 +0900

    docs: add comments

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,18 @@
  * Each field can be replaced for customizing the behavior.
  */
 export const config = {
+  /**
+   * Function that receives `error` and performs a nonlocal exit.
+   * At default it just rethrows `error`.
+   */
   onError: (error: Error): never => {
     throw error;
   },
+
+  /**
+   * Function that edits `error` before being passed to `onError()`.
+   * At default it appends `optionName` to the error message.
+   */
   editError: (error: Error, optionName: string): Error => {
     error.message += ` (option "${optionName}")`;
     return error;
