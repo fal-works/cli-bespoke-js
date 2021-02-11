3d846dc3158b1af3eae28fd1bc7d66d7c190a3ca
FAL <falworks.contact@gmail.com>
2021-02-11 11:40:16 +0900

    refactor: add eslint comment

--- src/convert/array.ts
+++ src/convert/array.ts
@@ -38,6 +38,8 @@ export const oneOrMore = <T>(
 ): readonly [T, ...T[]] => {
   if (values === undefined) sendError(new Error("Missing value"));
   if (values.length === 0) sendError(new Error("Missing value"));
+
+  // eslint-disable-next-line total-functions/no-unsafe-type-assertion
   return values as readonly [T, ...T[]];
 };
 
